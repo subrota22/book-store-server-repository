c4f35e3261e356c4890c83f5dbbf6f14edc57fda
Subrota Chandra Sarker <itinfobd24@gmail.com>
2023-03-01 03:11:51 +0600

    add all codes

--- src/app.js
+++ src/app.js
@@ -3,16 +3,213 @@ const express = require('express');
 const app  = express() ;
 const port = process.env.PORT || 4000; 
 const { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
-const graphQlSchema = require("./Schema/Schema");
- const graphQlResolvers = require("./resolver/index");
-var schema = require('./graphql/bookSchemas')
 const mongoose = require("mongoose").set("strictQuery", true);
 const mongoDB_url = process.env.mongodb_url;
 const cors = require("cors") ;
+//book schema
+var GraphQLSchema = require('graphql').GraphQLSchema;
+var GraphQLObjectType = require('graphql').GraphQLObjectType;
+var GraphQLList = require('graphql').GraphQLList;
+var GraphQLObjectType = require('graphql').GraphQLObjectType;
+var GraphQLNonNull = require('graphql').GraphQLNonNull;
+var GraphQLString = require('graphql').GraphQLString;
+var GraphQLInt = require('graphql').GraphQLInt;
+var GraphQLDate = require('graphql-date');
+//
 app.use(express.json()) ;
 app.use('*', cors());
- 
+ //book shema start <<---------------------<<-----------------<<
+const bookModelInfo = () => {
+  
+  var BookSchema = new mongoose.Schema({
+  id: String,
+  isbn: String,
+  title: String,
+  author: String,
+  description: String,
+  published_year:String ,
+  publisher: String,
+  updated_date: { type: Date, default: Date.now },
+});
+
+return mongoose.model("Book" , BookSchema) ;
+
+}
+const bookInfo = bookModelInfo() ;
+//book schema for graphQL
+function bookSchema () {
+var bookType = new GraphQLObjectType({
+  name: 'book',
+  fields: function () {
+    return {
+      _id: {
+        type: GraphQLString
+      },
+      isbn: {
+        type: GraphQLString
+      },
+      title: {
+        type: GraphQLString
+      },
+      author: {
+        type: GraphQLString
+      },
+      description: {
+        type: GraphQLString
+      },
+      published_year: {
+        type: GraphQLInt
+      },
+      publisher: {
+        type: GraphQLString
+      },
+      updated_date: {
+        type: GraphQLDate
+      }
+    }
+  }
+});
+
+
+
+
+var queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields:  function () {
+    return {
+      books: {
+        type: new GraphQLList(bookType),
+        resolve:async function () {
+          const books = await bookInfo.find().exec()
+          if (!books) {
+            throw new Error('Error')
+          }
+          return books
+        }
+      },
+      book: {
+        type: bookType,
+        args: {
+          id: {
+            name: '_id',
+            type: GraphQLString
+          }
+        },
+        resolve: async function (root, params) {
+          const bookDetails = await bookInfo.findById(params.id).exec();
+          if (!bookDetails) {
+            throw new Error('Error')
+          }
+          return bookDetails
+        }
+      }
+    }
+  }
+});
+
+
+var mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: function () {
+    return {
+      //add book 
+      addBook: {
+        type: bookType,
+        args: {
+          isbn: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          title: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          author: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          description: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          published_year: {
+            type: new GraphQLNonNull(GraphQLInt)
+          },
+          publisher: {
+            type: new GraphQLNonNull(GraphQLString)
+          }
+        },
+        resolve: async function  (root, params) {
+          const bookModel =  new bookInfo(params);
+          const newBook = await  bookModel.save();
+          if (!newBook) {
+            throw new Error('Error');
+          }
+          return newBook
+        }
+      },
+//update 
+      updateBook: {
+        type: bookType,
+        args: {
+          id: {
+            name: 'id',
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          isbn: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          title: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          author: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          description: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          published_year: {
+            type: new GraphQLNonNull(GraphQLInt)
+          },
+          publisher: {
+            type: new GraphQLNonNull(GraphQLString)
+          }
+        },
+        resolve: async function (root, params) {
+          await bookInfo.findByIdAndUpdate(params.id, { isbn: params.isbn, title: params.title, author: params.author, description: params.description, published_year: params.published_year, publisher: params.publisher, updated_date: new Date() }, function (err) {
+            if (err) return next(err);
+          });
+        }
+      },
+//remove book 
+      removeBook: {
+        type: bookType,
+        args: {
+          id: {
+            type: new GraphQLNonNull(GraphQLString)
+          }
+        },
+        resolve:async function(root, params) {
+          const remBook = await bookInfo.findByIdAndRemove(params.id).exec();
+          if (!remBook) {
+            throw new Error('Error')
+          }
+          return remBook;
+        }
+      } ,
+      
+    }
+  }
+});
+return  new GraphQLSchema({query:queryType, mutation: mutation });  ;
+}
+//>>------------------>>---------->> schema end 
+
+//graphQL connectin 
+const bookSchemaGet = bookSchema() ;
+app.use('/books', cors(), graphqlHTTP({
+  schema: bookSchemaGet,
+  rootValue: global,
+  graphiql: true,
+}));
+
+
 //database connection start with mongoDB 
 
 const options = { useNewUrlParser: true, useUnifiedTopology: true  }
@@ -23,24 +220,11 @@ mongoose
     throw error
   });
 
-//graphQL connectin 
 
-app.use('/books', cors(), graphqlHTTP({
-  schema: schema,
-  rootValue: global,
-  graphiql: true,
-}));
 
-//
 
-app.use(
-    "/grpahql",  cors() ,
-    graphqlHTTP({
-      schema: graphQlSchema,
-      rootValue:  graphQlResolvers,
-      graphiql:true,
-    })
-) ; 
+//database connection start with mongoDB 
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const client = new MongoClient(mongoDB_url, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const booksCollection = client.db("test").collection("books");
@@ -63,57 +247,73 @@ app.listen(port, (req, res) => {
 });
 
 
+///
 
 
 
 
 
-
-
-
-
-
-
-
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const client = new MongoClient(mongoDB_url, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-//   const booksCollection = client.db("book-store").collection("bookss");
-//database connection start with mongoDB 
-
-// const options = { useNewUrlParser: true, useUnifiedTopology: true  }
-// mongoose
-//   .connect(mongoDB_url, options)
-//   .then(() =>  console.log("Successfully connected with mongoDB !! "))
-//   .catch(error => {
-//     throw error
-//   }) 
-
-// // Construct a schema, using GraphQL schema language
-// var schema = buildSchema(`
-//   type Query {
-//     hello: String
+// var paginationType = new GraphQLObjectType({
+//   name: 'bookQuery',
+//   fields: function () {
+//     return {
+//       page: {
+//         type: GraphQLString
+//       },
+//       size: {
+//         type: GraphQLString
+//       }
+   
+//     }
 //   }
-// `);
-
-// // The root provides a resolver function for each API endpoint
-// var root = {
-//   hello: () => {
-//     return 'Hello world!';
-//   },
-// };
-
-
-// app.use('/graphql', graphqlHTTP({
-//   schema: schema,
-//   rootValue: root,
-//   graphiql: true,
-// }));
-
-
-// app.post("/books" , async (req ,res) => {
-// const data = req.body ;
-// const result = await booksCollection.insertOne(data) ;
-// res.send(result) ;
-// });
+//   });
+  
+//   var paginationQuery = new GraphQLObjectType({
+//   name: 'paginationQuery',
+//   fields:  function () {
+//     return {
+//       booksQuery: {
+//         type: new GraphQLList(paginationType),
+//         resolve:async function () {
+//           const data = await bookInfo.find();
+//           if (!data) {
+//             throw new Error('Error')
+//           }
+//           return data
+//         }
+//       },
+    
+//     }
+//     }
+//     }) ;
+  
+//     var paginationMutaion = new GraphQLObjectType({
+//       name: 'mutation',
+//       fields: function () {
+//         return {
+//           booksQuery: {
+//             type: paginationType,
+//             args: {
+//               page: {
+//                 type: new GraphQLNonNull(GraphQLString)
+//               },
+//               size: {
+//                 type: new GraphQLNonNull(GraphQLString)
+//               }
+//             },
+//             resolve: async function  (root, params) {
+//               const page = parseInt(params.page);
+//               const size = parseInt(params.size);
+//               console.log("books params ==>" , params);
+//               // ----> parametar  <---- //
+//               const count = await bookInfo.estimatedDocumentCount();
+//               const data = await bookInfo.find().skip(page * size).limit(size).sort({_id: -1 }).toArray();
+//               const paginationData = { count, data } ;
+  
+//               if (!paginationData) {
+//                 throw new Error('Error');
+//               }
+//               return paginationData
+//             }
+//           },
+//         }}}) ;
